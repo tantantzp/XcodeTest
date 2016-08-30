9564ac1157e9eb6dd3945a0d21480bf2f31fb5cb
靳超 <jinchao@haharangwokankan.local>
2016-08-30 10:18:35 +0800

    Initial Commit

---
+++ HelloXcode/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloXcode
+//
+//  Created by 靳超 on 16/8/30.
+//  Copyright © 2016年 tzp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
